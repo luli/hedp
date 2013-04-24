9deb5419e95548a36e720975a4e534b8279b526e
Roman Yurchak <rth@crans.org>
2013-04-24 22:37:59 +0200

    Refactored abel

--- hedp/__init__.py
+++ hedp/__init__.py
@@ -4,8 +4,8 @@
 # Roman Yurchak, Laboratoire LULI, 11.2012
 
 from . import io
-import abel
 import diags
 from matdb import matdb, MATDB_PATH
 
 import cst
+import pp
--- hedp/pp/abel.py
+++ hedp/pp/abel.py
@@ -8,8 +8,7 @@ import sys
 
 import numpy as np
 from scipy.integrate import simps
-from numpy.testing import assert_, assert_array_almost_equal
-from maths.derivative import gradient
+from hedp.maths.derivative import gradient
 
 
 
@@ -124,71 +123,17 @@ def abel_analytical_step(r, fr_z, r0, r1):
     fr_z = fr_z.reshape((-1,1))
     return F_1d*fr_z
 
-class TestAbel:
+def sym_abel_step_1d(r, r0, r1):
     """
-    To run tests run
-    nosetests -s abel.py
+    Produces a symmetrical analytical transform of a 1d step
     """
-    def test_timeit(self):
-        print '\nTimeit:'
-        n_list = [64, 128, 256]
-        for el in n_list:
-            x = np.random.randn(el,el)
-            t = time.time()
-            abel(x)
-            print '{0:>3d}x{0:>3d}  {1:>5.3f} s'.format(el, time.time() - t)
+    d = np.empty(r.shape)
+    for sens, mask in enumerate([r>=0, r<=0]):
+        d[mask] =  abel_analytical_step(np.abs(r[mask]), np.array(1), r0, r1)[0]
 
-    def test_zeros(self):
-        n = 64
-        x = np.zeros((n,n))
-        assert (abel(x, inverse=False)==0).all()
-
-    def test_step(self):
-        n = 512
-        fr = np.zeros((n/16,n))
-        r = 2e-3*np.arange(n)
-        r0, r1 = 0.2, 0.5
-        fr[:,(r>r0)*(r<r1)] = 3
-        Fn = abel(fr, r)
-        Fn_a = abel_analytical_step(3*np.ones(fr.shape[0]),r, r0,r1)
-        err =  np.abs(Fn - Fn_a)
+    return d
 
 
-
-
-
-    def test_inversion(self):
-        n = 256
-        fr = np.zeros((n,n)) 
-        X, Y = np.meshgrid(np.arange(n), np.arange(n))
-        idx = ((X**2+Y**2)<(0.6*n)**2) *  ((X**2+Y**2)>(0.4*n)**2)
-        idx = idx + ((X>0.8)*(X<0.85)*(Y>0.8)*(Y<0.85))
-        fr[idx] = 1
-        fr_hat = iabel(abel(fr))
-        err_cont = np.abs(fr - fr_hat)
-        err_assym = np.abs(fr_hat[:,0] - fr_hat[0,:])
-        r = 5e-3*np.arange(n) 
-        r += 3e-3*np.random.rand(n)
-        r.sort()
-        fr = np.abs(np.sinc(5*r/r.max()))
-        err_sin = np.abs(iabel(abel(fr, r), r) - fr)
-
-
-        print '\nConsecutive iabel->abel error '
-        print '               p25        p50        p75         p95         max'
-        for key, err in  dict(asymm=err_assym, continuous=err_cont,
-                                                    sin=err_sin).iteritems():
-            print '{5:13}{0:.3e}  {1:.3e}  {2:.3e}   {3:.3e}  {4:.3e}'.format(
-                    np.percentile(err, 25), np.percentile(err, 50),
-                    np.percentile(err, 75), np.percentile(err, 95),
-                    err.max(), key)
-    def test_gradient(self):
-        N = 5500
-        x = np.arange(N)/(0.1*N)
-        f = np.sin(x)
-        err = np.abs(gradient(f, x) - np.cos(x)).sum()/len(x)
-        assert err < 1e-5
-        print 'Gradient err for sin: ', err
 
 
 
