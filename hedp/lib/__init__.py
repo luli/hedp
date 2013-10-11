259b00f6a6838ea0e6d1b7feb2a2bbdd687f60c0
Roman Yurchak <rth@crans.org>
2013-10-11 18:05:38 +0200

    Mooved under CeCILL licence

--- hedp/lib/__init__.py
+++ hedp/lib/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
+# Copyright CNRS 2012, roman.yurchak@polytechnique.edu
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding by the rules of distribution of free software.
 
--- hedp/lib/integrators.pyx
+++ hedp/lib/integrators.pyx
@@ -3,6 +3,12 @@
 # cython: cdivision=True
 # cython: wraparound=False
 ## cython: profile=True
+#
+# Copyright CNRS 2012, roman.yurchak@polytechnique.edu
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding by the rules of distribution of free software.
+
 import numpy as np
 cimport numpy as np
 from libc.math cimport sqrt, acosh
